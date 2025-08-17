5e579c0ea3b574c9d752bdfdebe57e271a2351ef
Anil Kumar <anilkumarkaturi@gmail.com>
2025-08-17 22:11:13 +0530

    all changes made in model classes

--- job-portal/src/main/java/com/job_portal/model/JobCompany.java
+++ job-portal/src/main/java/com/job_portal/model/JobCompany.java
@@ -3,15 +3,20 @@ package com.job_portal.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Table(name="job_company")
+@Table(name = "job_company")
 @Entity
 @Data
 public class JobCompany {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "logo")
     private String logo;
+
+    @Column(name = "name")
     private String name;
 
 }
--- job-portal/src/main/java/com/job_portal/model/JobSeekerProfile.java
+++ job-portal/src/main/java/com/job_portal/model/JobSeekerProfile.java
@@ -3,30 +3,38 @@ package com.job_portal.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Table(name="job_seeker_profile")
+@Table(name = "job_seeker_profile")
 @Entity
 @Data
 public class JobSeekerProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_account_id")
+    @Column(name = "user_account_id")
     private Integer userAccountId;
-    @Column(name="user_account_id")
+
+    @Column(name = "city")
     private String city;
-    @Column(name="country")
+
+    @Column(name = "country")
     private String country;
-    @Column(name="first_name")
+
+    @Column(name = "first_name")
     private String firstName;
-    @Column(name="last_name")
+
+    @Column(name = "last_name")
     private String lastName;
-    @Column(name="profile_photo")
+
+    @Column(name = "profile_photo")
     private String profilePhoto;
-    @Column(name="resume")
+
+    @Column(name = "resume")
     private String resume;
-    @Column(name="state")
+
+    @Column(name = "state")
     private String state;
-    @Column(name="work_authorization")
+
+    @Column(name = "work_authorization")
     private String workAuthorization;
 
 }
